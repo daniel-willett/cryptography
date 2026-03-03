c6788aaffaffbdba3d33ab35771bfc0347497b07
Daniel Willett - Mint Laptop <willettdr1@gmail.com>
2026-03-03 11:40:22 +0000

    some work - to be carried on later

--- hashing/md5.go
+++ hashing/md5.go
@@ -2,6 +2,14 @@ package main
 
 import ("fmt")
 
+func padding(data []byte) []byte{
+	//Each []byte is 256 bits as []byte takes values 0-255
+	bitLength := len(data) * 256
+}
+
+
 func main(){
-	var input string = "Hello World"
+	input := "Hello World"
+	data := []byte(input)
+	data = padding(data)
 }
