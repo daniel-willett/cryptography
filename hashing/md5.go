6a0d0f731571ca928fb07227a5ddb7366c51c958
Daniel Willett <drw@netcraft.com>
2026-03-05 09:35:13 +0000

    Padding complete I am using https://www.comparitech.com/blog/information-security/md5-algorithm-with-examples/ to work from and have an example to compare with

--- hashing/md5.go
+++ hashing/md5.go
@@ -1,15 +1,46 @@
 package main
 
-import ("fmt")
+import ("fmt"; "math")
 
 func padding(data []byte) []byte{
 	//Each []byte is 8 bits as []byte takes values 0-255
 	bitLength := len(data) * 8
+	var quotient int = bitLength / 512
+	paddingNeeded := ((quotient+1)*512) - bitLength
+	numberOfZeros := paddingNeeded - 64
+	//First "chunk" (8bits) is taken by binary 10000000 which is 128 in decimal
+	data = append(data, 128)
+	for i:=1; i<=(numberOfZeros/8)-1; i++{ //We need all the "chunks" except the first one which has just been done
+		data = append(data, 0)
+	}
+	lengthPaddingValue := bitLength%int(math.Pow(2,64))
+	lengthPadding := make([]byte, 64/8) //64bit but each "chunk" is 8bits so we need 64/8=8 "chunks"
+	//[0     0     0     0     0     0     0     0]
+	//2^8^7 2^8^6 2^8^5 2^8^4 2^8^3 2^8^2 2^8^1 2^8^0
+	fmt.Println(lengthPaddingValue)
+	for i:=7; i>=0; i--{
+		var temp int = lengthPaddingValue / int(math.Pow(2,float64(8*i)))
+		fmt.Println("Temp is %v", temp)
+		lengthPadding[7-i] = byte(temp)
+		lengthPaddingValue = lengthPaddingValue - (temp*int(math.Pow(2,float64(8*i))))
+		fmt.Println("lengthPaddingValue is %v", lengthPaddingValue)
+	}
+	fmt.Println(lengthPadding)
+	data = append(data, lengthPadding...)
+	return data
 }
 
 
 func main(){
+	/*
 	input := "Hello World"
 	data := []byte(input)
 	data = padding(data)
+	fmt.Println(data)
+	*/
+
+	input := "They are deterministic."
+	data := []byte(input)
+	data = padding(data)
+	fmt.Println(data)
 }
