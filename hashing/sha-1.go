d9e20fe9680fd4432ff1910782d73a8ce8018cee
Daniel Willett - Mint Laptop <willettdr1@gmail.com>
2026-03-16 23:00:46 +0000

    a bit of the algorithm

--- hashing/sha-1.go
+++ hashing/sha-1.go
@@ -1,6 +1,6 @@
 package main
 
-import ("fmt"; "hashing/util"; "os")
+import ("fmt"; "hashing/util"; "math/bits"; "os")
 
 func main(){
 	args := os.Args
@@ -18,7 +18,7 @@ func main(){
 
 
         data = util.Padding(data, false)
-	fmt.Println(data)
+	//Everything for SHA-1 is Big Endian
 
 	/*
         The starting values are the following turned into little endian which I have done manually
@@ -28,7 +28,6 @@ func main(){
         0x76543210
 	0xF0E1D2C3
         */
-	/*
         var initialA uint32 = 0x67452301
         var initialB uint32 = 0xefcdab89
         var initialC uint32 = 0x98badcfe
@@ -42,5 +41,17 @@ func main(){
 	var E uint32 = initialE
 
         var new_A, new_B, new_C, new_D, new_E uint32 = 0, 0, 0, 0, 0
-	*/
+
+	for i:=0;i<len(data)/64;i++{
+		M:=data[64*i:64*(i+1)] //512-bit block
+		words := make([][]byte,16)
+		for j:=[0];j<16;j++{
+			words[j] = M[4*j:4*(j+1)]
+		}
+		for j:=16;j<80;j++{
+			words[j] = bits.RotateLeft32((words[j-3] ^ words[j-8] ^ words[j-14] ^ words[j-16]), 1)
+		}
+
+
+	}
 }
